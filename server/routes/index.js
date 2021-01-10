07b7e6979700d8a43e859d8f77c01f260a461483
Felipe Glaría Grego <fglaria@gmail.com>
2021-01-10 17:16:44 -0300

    reordering

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var router = express.Router();
-
 var path = require('path');
+
+var router = express.Router();
 
 /* GET home page 1. */
 router.get('/', function(req, res, next) {

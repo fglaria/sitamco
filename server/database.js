36fd647d0fe671e75670c92798b84d25f791a60b
Felipe Glaría Grego <fglaria@gmail.com>
2021-01-10 11:51:49 -0300

    database removed commented string

--- server/database.js
+++ server/database.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 require('dotenv').config();
 
-// var connectionString = `mongodb+srv://${host}:${port}/${database}?retryWrites=true&w=majority`;
-var options = { useNewUrlParser: true, useUnifiedTopology: true }
+var options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 module.exports = () => mongoose.connect(process.env.DB_CONNECTION_STRING, options);
